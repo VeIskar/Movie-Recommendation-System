6af431d12201ce58059796e24d8898e0a118753a
VeIskar <mlewandowski380@gmail.com>
2024-08-28 23:26:16 +0200

    add: tf-idf title based recommendation

--- app.py
+++ app.py
@@ -1,22 +1,21 @@
 from flask import Flask, request, jsonify, render_template
 import pandas as pd
+from sklearn.neighbors import NearestNeighbors
+from sklearn.feature_extraction.text import TfidfVectorizer
+from scipy.sparse import csr_matrix
+from sklearn.metrics.pairwise import cosine_similarity
+
 
 app = Flask(__name__)
 
-
 mov_df = pd.read_csv('data/movies.csv')
 rat_df = pd.read_csv('data/ratings.csv')
-tag_df = pd.read_csv('data/tags.csv')
-lin_df = pd.read_csv('data/links.csv')
 
 print("Successfully loaded dataset")
 
+mov_df['genre_list'] = mov_df['genres'].str.split('|')
 
-joined_data = rat_df.join(mov_df.set_index('movieId')['genres'], on='movieId')
-joined_data = joined_data.join(mov_df.set_index('movieId')['title'], on='movieId')
-
-
-def get_recommendations(user_filter=100):
+def get_recommendations(user_filter):
     rat_amount = rat_df.groupby(['movieId']).count()
     rat_mean = rat_df.groupby(['movieId']).mean()
     rat_avg = rat_mean.loc[rat_amount['rating'] > user_filter]
@@ -27,6 +26,31 @@ def get_recommendations(user_filter=100):
     recommendations = recommendations[recommendations.columns[3:]]
     
     return recommendations
+
+vectorizer = TfidfVectorizer(ngram_range=(1, 2))
+tfidf = vectorizer.fit_transform(mov_df["title"])
+
+#Content-Based recommendation based on movie title similarity using tf-idf and cosine measure
+def recom_tfid_title(movie_title):
+    try:
+        query_index = mov_df[mov_df['title'].str.contains(movie_title, case=False)].index[0]
+    except IndexError:
+        return pd.DataFrame()
+    
+    sim_scores = cosine_similarity(tfidf[query_index], tfidf).flatten()
+    sim_indices = sim_scores.argsort()[-11:][::-1]
+    
+    sim_movs_tl = mov_df.iloc[sim_indices]
+    sim_movs_tl = sim_movs_tl[sim_movs_tl['title'] != movie_title]
+    
+    return sim_movs_tl.head(10)
+
+
+@app.route('/recommend/tfidf', methods=['POST'])
+def recommend_tfidf():
+    movie_title = request.form.get('tfidf-mov', type=str)
+    recommendations = recom_tfid_title(movie_title)
+    return jsonify(recommendations.to_dict(orient='index'))
 
 @app.route('/')
 def index():
--- static/script.js
+++ static/script.js
@@ -23,3 +23,44 @@ document.getElementById('recommendation-form').addEventListener('submit', functi
         }
     });
 });
+
+document.getElementById('recommendation-tfidf-movie').addEventListener('submit', function(event) {
+    event.preventDefault();
+    const movieTitle = document.getElementById('tfidf-mov-search').value;
+
+    fetch('/recommend/tfidf', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: `tfidf-mov=${movieTitle}`,
+    })
+    .then(response => response.json())
+    .then(data => {
+        const recommendationsDiv = document.getElementById('recommendations-tfidf');
+        recommendationsDiv.innerHTML = '<h2>Recommended Movies:</h2>';
+        for (let movieId in data) {
+            let movie = data[movieId];
+            let movieDiv = document.createElement('div');
+            movieDiv.innerHTML = `<strong>${movie.title}</strong> (${movie.genres})`;
+            recommendationsDiv.appendChild(movieDiv);
+        }
+    });
+});
+
+
+$(function() {
+    $("#tfidf-mov-search").autocomplete({
+        source: function(request, response) {
+            $.ajax({
+                url: "/autocomplete/movies" ,
+                data: { query: request.term}, 
+                success: function(data) {
+                    response(data);
+                }
+            });
+        },
+        minLength: 2
+
+    }); 
+});
